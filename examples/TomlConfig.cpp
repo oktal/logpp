b67009c00d5601cd1d73c9586d331036c42bc504
Mathieu Stefani <auquetal@gmail.com>
2021-02-14 16:02:51 +0100

    core(config): Start using FileWatcher to dynamically reload configuration (TOML)

--- examples/TomlConfig.cpp
+++ examples/TomlConfig.cpp
@@ -1,6 +1,8 @@
 #include "logpp/config/TomlConfigurator.h"
 #include "logpp/logpp.h"
 
+#include "logpp/config/FileWatcher.h"
+
 #include <iostream>
 
 int main(int argc, const char* argv[])
@@ -14,6 +16,12 @@ int main(int argc, const char* argv[])
 
     // Let's configure our registry from a TOML configuration file.
     auto err = logpp::TomlConfigurator::configureFile(file);
+
+    // Uncomment the lines below to automatically watch the file and update the configuration.
+    // auto watcher = std::make_shared<logpp::FileWatcher>();
+    // watcher->start();
+    // auto [watchId, err] = logpp::TomlConfigurator::configureFileAndWatch(file, watcher);
+
     if (err)
     {
         std::cerr << "Error configuring logger: " << *err << std::endl;
@@ -27,4 +35,7 @@ int main(int argc, const char* argv[])
     logger->info("This is an informational message",
         logpp::field("exe_name", argv[0])
     );
+
+    std::cout << "Press a key to exit.\n";
+    std::cin.get();
 }
