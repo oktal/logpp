1e94636027d84d5405c2c1ef6cb2bcc20077c51f
Mathieu Stefani <auquetal@gmail.com>
2021-04-19 13:11:31 +0200

    workflows: Start preparing release workflow

---
+++ tools/version.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -eu
+
+SEMVER_REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+make version | grep -E ${SEMVER_REGEX}
