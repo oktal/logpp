d56f21e05bf4768b5acb80653b5de774d59aeebe
Mathieu Stefani <auquetal@gmail.com>
2021-04-26 14:36:29 +0200

    chore: Bump patch version number to 0.1.1

--- tools/check-version.sh
+++ tools/check-version.sh
@@ -4,14 +4,10 @@ set -eu
 
 SEMVER_REGEX='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 
-TAG_PREFIX=${2:-v}
-TAG=$1
-
-TAG_VERSION=${TAG/#$TAG_PREFIX}
-
+TARGET_VERSION=$1
 VERSION=`make version | grep -E ${SEMVER_REGEX}`
 
-if [ "$TAG_VERSION" != "$VERSION" ]
+if [ "$TARGET_VERSION" != "$VERSION" ]
 then
     echo FAILED
     exit 1
